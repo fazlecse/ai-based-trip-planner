13a266bdaedd6012705327c83c519e0007f8fedd
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2025-01-27 18:55:12 +0600

    rtl done, all js, css remove

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -57,85 +57,25 @@ function formatState(state) {
 
 $(document).ready(function () {
   // offer start
-  var swiper = new Swiper(".offer-swiper", {
-    centeredSlides: true,
-    effect: "fade",
-    autoplay: {
-      false: true,
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    autoplay: false,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  // offer slider end
-
-  //Multi step progress section start
-
-  // Jquery UI start
-  if ($("#datepicker").length) {
-    $("#datepicker").datepicker({
-      buttonImageOnly: false,
+  if ($(".offer-swiper").length) {
+    var swiper = new Swiper(".offer-swiper", {
+      centeredSlides: true,
+      effect: "fade",
+      autoplay: {
+        false: true,
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      autoplay: false,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     });
   }
-  // Jquery UI end
-
-  // Apexcharts start
-  // Piechart
-  if ($("#pieChart").length) {
-    var options = {
-      series: [44, 55, 13, 33],
-      chart: {
-        type: "donut",
-        height: "300",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        position: "right",
-        offsetY: 0,
-        height: 230,
-      },
-    };
-
-    var chart = new ApexCharts(document.querySelector("#pieChart"), options);
-    chart.render();
-  }
-  // Apexcharts end
+  // offer slider end
 });
-
-// Dark theme start
-const toggleBtn = document.getElementById("toggle-btn");
-const body = document.querySelector("body");
-toggleBtn.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    localStorage.setItem("dark-theme", 1);
-  } else {
-    localStorage.setItem("dark-theme", 0);
-  }
-  setTheme();
-});
-
-function setTheme() {
-  const isDarkTheme = localStorage.getItem("dark-theme");
-  if (isDarkTheme == 1) {
-    document.querySelector("body").classList.add("dark-theme");
-    document.getElementById("moon").style.display = "none";
-    document.getElementById("sun").style.display = "block";
-  } else {
-    document.querySelector("body").classList.remove("dark-theme");
-    document.getElementById("moon").style.display = "block";
-    document.getElementById("sun").style.display = "none";
-  }
-}
-setTheme();
-// Dark theme end
